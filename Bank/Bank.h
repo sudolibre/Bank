166ba2030405bbb147b5d13555536b74536e8278
sudolibre <optout.breaker@gmail.com>
2016-12-20 16:49:38 -0500

    Initial Commit

---
+++ Bank/Bank.h
@@ -0,0 +1,19 @@
+//
+//  Bank.h
+//  Bank
+//
+//  Created by Jonathon Day on 12/20/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bank.
+FOUNDATION_EXPORT double BankVersionNumber;
+
+//! Project version string for Bank.
+FOUNDATION_EXPORT const unsigned char BankVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bank/PublicHeader.h>
+
+
